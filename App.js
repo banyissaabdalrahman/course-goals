6f306cfe847f0c97eb298814b4a88acba8d1183c
Layan khater <lkhater@itgsoftware.com>
2022-08-03 16:31:15 +0300

    modifing app.js

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.root}>
-     <Text>Hello World!!!</Text>
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder="Your course goal!" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals ...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  root:{
-    padding:50,
-  }
+  appContainer: {
+    padding: 50,
+    flex: 1,
+  },
 });
